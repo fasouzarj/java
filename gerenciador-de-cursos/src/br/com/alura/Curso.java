c2b81118af916b1bf50d449af003df111d4b7d00
Flavio Alves <flavio.alves84@gmail.com>
2023-02-22 11:23:30 -0300

    Collections.sort

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -6,29 +6,47 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Curso {
-    private String nome;
-    private String instrutor;
-    private List<Aula> aulas = new LinkedList<Aula>();
+	private String nome;
+	private String instrutor;
+	private List<Aula> aulas = new LinkedList<Aula>();
 
-    public Curso(String nome, String instrutor) {
-        this.nome = nome;
-        this.instrutor = instrutor;
-    }
+	public Curso(String nome, String instrutor) {
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getInstrutor() {
-        return instrutor;
-    }
+	public String getInstrutor() {
+		return instrutor;
+	}
 
-    public List<Aula> getAulas() {
-        return Collections.unmodifiableList(aulas);
-    }
+	public List<Aula> getAulas() {
+		return Collections.unmodifiableList(aulas);
+	}
 
-    public void adiciona(Aula aula) {
-        this.aulas.add(aula);
-    }
+	public void adiciona(Aula aula) {
+		this.aulas.add(aula);
+	}
 
+	// public int getTempoTotal() {
+	// int tempoTotal = 0;
+	// for (Aula aula : aulas) {
+	// tempoTotal += aula.getTempo();
+	// }
+	// return tempoTotal;
+	// }
+
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+	@Override
+	public String toString() {
+		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", "
+				+ "aulas: " + this.aulas + "]";
+
+	}
 }
